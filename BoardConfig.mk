0447bd67dd16d5ac657f6ee6adbd6992c042e9e6
Enceka <72303957+enceka@users.noreply.github.com>
2025-05-12 21:35:42 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/zte/U30Air
+DEVICE_PATH := device/zte/u30air
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
